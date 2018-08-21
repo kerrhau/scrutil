6a514ecc12e4971474433e7119771d669be35399
Ahmed Mohamed <amohamed5083@gmail.com>
2018-08-21 07:17:23 -0500

    fixed swapped R&B channels
    fixed swapped R&B channels
    
    fixed swapped R&B channels

--- src/png_handler.cpp
+++ src/png_handler.cpp
@@ -1,5 +1,7 @@
 #include <png.h>
 #include <iostream>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
 void png_write(png_structp png_ptr, png_bytep data, png_size_t length) {
     std::ostream *stream = (std::ostream*)png_get_io_ptr(png_ptr);
@@ -11,7 +13,7 @@ void png_flush(png_structp png_ptr) {
     stream->flush();
 }
 
-void writePng(std::ostream& stream, int width, int height, char* data) {
+void write_png(std::ostream& stream, int width, int height, char *data) {
     png_structp png_ptr = nullptr;
     png_infop info_ptr = nullptr;
     png_bytep *rows = new png_bytep[height];
@@ -19,20 +21,28 @@ void writePng(std::ostream& stream, int width, int height, char* data) {
     png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
 
     if (!png_ptr)
-        throw new std::runtime_error("[writePng] png_create_write_struct failed; could not initialize png_ptr");
+        throw new std::runtime_error("[write-png] png_create_write_struct failed; could not initialize png_ptr");
 
     info_ptr = png_create_info_struct(png_ptr);
 
     if (!info_ptr)
-        throw new std::runtime_error("[writePng] png_create_info_struct failed; could not initialize info_ptr");
+        throw new std::runtime_error("[write_png] png_create_info_struct failed; could not initialize info_ptr");
 
     png_set_write_fn(png_ptr, &stream, png_write, png_flush);
     png_set_compression_level(png_ptr, 9);
 
     png_set_IHDR(png_ptr, info_ptr, width, height, 8,
                  PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE,
-                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+                 PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
 
+    for(int i = 0; i < (width * height) * 4; i += 4) {
+        char red = data[i + 2]; // B G R -> R G B 
+        char blue = data[i];
+
+        data[i] = red;
+        data[i + 2] = blue;
+    }
+    
     for(int i = 0; i < height; i++) {
         rows[i] = (png_bytep)(data + (i * width * 4));
     }
@@ -40,6 +50,5 @@ void writePng(std::ostream& stream, int width, int height, char* data) {
     png_write_info(png_ptr, info_ptr);
     png_write_image(png_ptr, rows);
     png_write_end(png_ptr, info_ptr);
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-    
+    png_destroy_write_struct(&png_ptr, &info_ptr); 
 }
--- src/png_handler.h
+++ src/png_handler.h
@@ -1,3 +1,3 @@
 void png_write(png_structp png_ptr, png_bytep data, png_size_t length); 
 void png_flush(png_structp png_ptr);
-void writePng(std::ostream& stream, int width, int height, char* data);
+void write_png(std::ostream& stream, int width, int height, char* data);
